8d270433f88fe8f1e6db1790525d53cd71d123a4
ArihantAccionLabs <32539325+ArihantAccionLabs@users.noreply.github.com>
2017-12-07 20:48:24 +0530

    AWS Api Links Changed in Profile Information Page

--- src/consumer-app/app/services/profile-info.service.ts
+++ src/consumer-app/app/services/profile-info.service.ts
@@ -15,7 +15,7 @@ export class ProfileInfoService {
 
     /*Profile Completion API*/
     completeProfile(profileInformation) {
-        this.url = 'http://dev-consumer-api.us-east-2.elasticbeanstalk.com/profile/userDetail/';
+        this.url = 'http://dev-consumer-profile.us-east-2.elasticbeanstalk.com/profile/userDetail/';
         return this.http.post(this.url, profileInformation).map((res) => res.text())
     }
 }
