d04e3d0c55184a1556dd0741f68faa070b3fc397
ArihantAccionLabs <32539325+ArihantAccionLabs@users.noreply.github.com>
2018-01-04 17:13:56 +0530

    Local Environment File Changes For Consumer

--- src/consumer-app/environments/environment.local.ts
+++ src/consumer-app/environments/environment.local.ts
@@ -2,14 +2,15 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-import { apiNames, commonMessages } from './common';
-import { regexPatterns } from '../../common/regexPatterns';
+import { apiNames, commonMessages, regexPatterns } from './common';
 
 export const environment = {
   production: false,
-  Api: 'http://localhost:8081',
-  AdminApi: 'http://localhost:8083',
-  productApi: 'http://localhost:8086',
+  login: 'http://dev-user-service.us-east-2.elasticbeanstalk.com',
+  userService: 'http://dev-user-signup.us-east-2.elasticbeanstalk.com',
+  profileInterest: 'http://dev-consumer-profile.us-east-2.elasticbeanstalk.com',
+  productList: 'http://dev-product-service.us-east-2.elasticbeanstalk.com/api/products',
+  geoCode: 'https://maps.googleapis.com/maps/api/geocode/json',
   apis: apiNames,
   regex: regexPatterns,
   commonMsg: commonMessages
