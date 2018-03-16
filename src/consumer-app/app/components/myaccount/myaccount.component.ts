53edcffe30289dc5ae14b6716d55cf1b011cb540
Siva Nomula <siva.nomula@accionlabs.com>
2018-03-16 13:22:26 +0530

    stripe placeholder

--- src/consumer-app/app/components/myaccount/myaccount.component.ts
+++ src/consumer-app/app/components/myaccount/myaccount.component.ts
@@ -174,7 +174,7 @@ export class MyaccountComponent implements OnInit, AfterViewInit, OnDestroy {
     this.cardNumber = elements.create('cardNumber', { style: elementStyles, classes: elementClasses, });
     this.cardExpiry = elements.create('cardExpiry', { style: elementStyles, classes: elementClasses, });
     this.cardCvc = elements.create('cardCvc', { style: elementStyles, classes: elementClasses, });
-    this.cardZip = elements.create('postalCode', { style: elementStyles, classes: elementClasses, });
+    this.cardZip = elements.create('postalCode', { style: elementStyles, classes: elementClasses, placeholder: 'Postal code', });
 
     // this.card.mount(this.cardInfo.nativeElement);
     this.cardNumber.mount(this.cardNumberInfo.nativeElement);
@@ -187,6 +187,7 @@ export class MyaccountComponent implements OnInit, AfterViewInit, OnDestroy {
         this.setBrandIcon(brand);
       }
     });
+    this.cardZip.placeholder = 'postalCode';
   }
 
   setBrandIcon(brand) {
